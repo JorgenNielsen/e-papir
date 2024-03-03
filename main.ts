d4883714a8af0e3afea96b52cd9e10409e3276ba
JorgenNielsen <50008532+JorgenNielsen@users.noreply.github.com>
2024-03-03 11:29:44 +0100

    De første filer til MakeCode-projekt

---
+++ main.ts
@@ -0,0 +1,28 @@
+basic.showNumber(0)
+inkybit.clear()
+inkybit.drawText(
+"Hello world",
+0,
+0,
+inkybit.Color.Black,
+inkybit.TextSize.Regular
+)
+inkybit.drawIcon(
+IconNames.Heart,
+106,
+48,
+inkybit.Color.Black,
+inkybit.TextSize.Regular
+)
+inkybit.drawRectangle(
+0,
+50,
+119,
+20,
+inkybit.Color.Black,
+false
+)
+inkybit.show()
+basic.forever(function () {
+	
+})
