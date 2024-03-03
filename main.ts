9698ea86e52d366485753d370d5f94f52173db36
JorgenNielsen <50008532+JorgenNielsen@users.noreply.github.com>
2024-03-03 17:58:54 +0100

    mange

--- main.ts
+++ main.ts
@@ -1,16 +1,44 @@
-basic.showNumber(0)
+function sprite () {
+    inkybit.drawIcon(
+    IconNames.Heart,
+    x,
+    y,
+    inkybit.Color.Black,
+    inkybit.TextSize.Regular
+    )
+    inkybit.show()
+    y += 1
+    basic.pause(200)
+}
+input.onButtonPressed(Button.A, function () {
+    inkybit.clear()
+    inkybit.drawText(
+    "Jørgen",
+    0,
+    0,
+    inkybit.Color.Black,
+    inkybit.TextSize.Regular
+    )
+})
+input.onButtonPressed(Button.B, function () {
+    inkybit.clear()
+    inkybit.drawText(
+    "Oscar",
+    0,
+    0,
+    inkybit.Color.Black,
+    inkybit.TextSize.Regular
+    )
+})
+let y = 0
+let x = 0
+x = 0
+y = 0
 inkybit.clear()
 inkybit.drawText(
 "Hello world",
-0,
-0,
-inkybit.Color.Black,
-inkybit.TextSize.Regular
-)
-inkybit.drawIcon(
-IconNames.Heart,
-106,
-48,
+x,
+y,
 inkybit.Color.Black,
 inkybit.TextSize.Regular
 )
